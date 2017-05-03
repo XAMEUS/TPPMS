53eae436cf9264e76bb6013aa56fc3d6527163bb
Maxime <maxime.gourgoulhon@gmail.com>
2017-05-03 19:54:05 +0200

    q.13

--- script.r
+++ script.r
@@ -1,2 +1,50 @@
+
+enumerate <- function(X, FUN, ...) {
+  result <- vector("list", length(X))
+  for (i in seq_along(result)) {
+    tmp <- FUN(X[[i]], i, ...)
+    if (is.null(tmp))
+      result[i] <- list(NULL)
+    else
+      result[[i]] <- tmp
+  }
+  result
+}
+
+###############
+
 groupe1 = scan("./echantillons/groupe1.txt", what=integer(), sep="\n")
 groupe2 = scan("./echantillons/groupe2.txt", what=integer(), sep="\n")
+
+r = 1
+p = 0.5 # TODO
+q = 1 - p
+F<-function(k)
+{
+  # return (p / q * (1 - q ** (k + 1)) / (1 - q))
+  return ((1 - (1 - p) ** (k + 1)) / (1 - p))
+}
+
+n = 10
+m = 5
+k = seq(1,5,1)
+plot(k, F(k))
+lines(k, F(k), type="s")
+
+h<-function(x)
+{
+  return (log(1 - x))
+}
+
+x = sort(groupe1)
+y = x
+for (i in 1:length(x))
+{
+  y[i] = h(i / length(x))
+}
+plot(x, y)
+x = head(x, -1)
+y = head(y, -1)
+reg<-lm(y~x)
+lines(abs, fitted.values(reg))
+
