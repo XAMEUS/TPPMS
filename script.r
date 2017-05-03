cbf071b1ee2a4f6899a429fae4cef4825e5b2f0e
Maxime <maxime.gourgoulhon@gmail.com>
2017-05-03 18:16:45 +0200

    use scan

--- script.r
+++ script.r
@@ -1,2 +1,2 @@
-groupe1 = read.table("./echantillons/groupe1.txt")
-groupe2 = read.table("./echantillons/groupe2.txt")
+groupe1 = scan("./echantillons/groupe1.txt", what=integer(), sep="\n")
+groupe2 = scan("./echantillons/groupe2.txt", what=integer(), sep="\n")
