5cd7272043c1731a10ff29c1772dcc82e93eec9b
Nolwenn <nolwenn.cadic@gmail.com>
2017-05-04 16:07:08 +0200

    partie 2.1, 2.2

--- script.r
+++ script.r
@@ -28,7 +28,7 @@ F<-function(k)
 n = 10
 m = 5
 k = seq(1,5,1)
-plot(k, F(k))mi
+plot(k, F(k))
 
 lines(k, F(k), type="s")
 
@@ -49,3 +49,53 @@ y = head(y, -1)
 reg<-lm(y~x)
 lines(x, fitted.values(reg))
 
+####################
+# Question 2.1
+####################
+groupe1_ordonne = sort(groupe1)
+groupe2_ordonne = sort(groupe2)
+
+# minimum, 1er quartile, médianne, 
+#moyenne, 3ème quartile et max de groupe1
+summary(groupe1)
+
+# minimum, 1er quartile, médianne, 
+#moyenne, 3ème quartile et max de groupe1
+summary(groupe2)
+
+# Histogramme à classes de même largeur groupe1
+n1 = length(groupe1_ordonne)
+k = round(1 + log2(n1))
+a0 <- min(groupe1_ordonne) - 0.025*(max(groupe1_ordonne)-min(groupe1_ordonne))
+ak<-max(groupe1_ordonne)+0.025*(max(groupe1_ordonne)-min(groupe1_ordonne))
+bornes <- seq(a0, ak, (ak - a0)/k)
+hist(groupe1_ordonne, prob=T, breaks=bornes)
+
+
+# Histogramme à classes de même effectif
+
+histoeff <- function(x, xlim=NULL, ...)
+{
+  sx <- sort(x)
+  n <- length(x)
+  k <- round(log(n)/log(2)+1)
+  rangex <- max(x)-min(x)
+  breaks <- c(min(x)-0.025*rangex, quantile(x, seq(1, k-1)/k), max(x)+0.025*rangex)
+  col <- 0
+  if (is.null(xlim)) xlim<-c(breaks[1], breaks[k+1])
+  hist(x, breaks=breaks, col=col, xlim=xlim, probability=T, ...)
+}
+
+#Histogramme à classe de même effectif groupe1
+histoeff(groupe1)
+
+# Histogramme à classe de même largeur groupe2
+n2 = length(groupe2_ordonne)
+k = round(1+log2(n2))
+a0 <- min(groupe2_ordonne) - 0.025*(max(groupe2_ordonne)-min(groupe2_ordonne))
+ak<-max(groupe2_ordonne)+0.025*(max(groupe2_ordonne)-min(groupe2_ordonne))
+bornes <- seq(a0, ak, (ak - a0)/k)
+hist(groupe2_ordonne, prob=T, breaks=bornes)
+
+#Histogramme à classe de même effectif groupe2
+histoeff(groupe2)
