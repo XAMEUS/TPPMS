aea7ced4cca063a6c7beb7eb3a77cdfcf89fe809
Pierre Bouvier <Pierre.Bouvier1@ensimag.grenoble-inp.fr>
2017-05-05 14:50:34 +0200

    Partie1

--- partie1.r
+++ partie1.r
@@ -49,22 +49,29 @@ print(1 - exp(reg$coefficients[2]))
 
 # Second cas
 
-n = 10000
-r = 2000
-p = 0.9
-echantillon = sort(rnbinom(n,r,p))
-for (i in 1:n) {
-    echantillon[i] = echantillon[i] + r
-}
-liste_x = c()
-liste_y = c()
-
-for (i in 1:length(echantillon)-1) {
-    if (length(echantillon[echantillon==echantillon[i]+1])) {
-        liste_x = c(liste_x, echantillon[i])
-        liste_y = c(liste_y, c(echantillon[i] * length(echantillon[echantillon==echantillon[i]])  / length(echantillon[echantillon==echantillon[i]+1])))
+n = 100
+r = 20
+p = 0.1
+simul_second <-function(n, r, p) {
+    echantillon = sort(rnbinom(n,r,p))
+    for (i in 1:n) {
+        echantillon[i] = echantillon[i] + r
     }
+    liste_x = c()
+    liste_y = c()
+
+    for (i in 1:length(echantillon)-1) {
+        if (length(echantillon[echantillon==echantillon[i]+1])) {
+            liste_x = c(liste_x, echantillon[i])
+            liste_y = c(liste_y, c(echantillon[i] * length(echantillon[echantillon==echantillon[i]])  / length(echantillon[echantillon==echantillon[i]+1])))
+        }
+    }
+    return(list(x=liste_x, y=liste_y))
 }
+res = simul_second(n, r, p)
+liste_x = res$x
+liste_y = res$y
+
 plot(liste_x, liste_y)
 n_reg<-lm(liste_y~liste_x)
 print(n_reg)
@@ -73,3 +80,24 @@ print("pg2")
 print(1 - 1/ n_reg$coefficients[2])
 print("pg3")
 print(1 + (r-1) / n_reg$coefficients[1])
+
+# Test de pg2 et pg3
+listpg2 = c()
+listpg3 = c()
+for(r in seq(10, 1000, 50)) {
+    pg2 = 0
+    pg3 = 0
+    y = 50
+    for(j in 1:y) {
+        res = simul_second(n, r, p)
+        reg<-lm(res$y~res$y)
+        pg2 = pg2 + 1 - 1/ n_reg$coefficients[2]
+        pg3 = pg3 + 1 + (r-1) / n_reg$coefficients[1]
+    }
+    pg2 = pg2 / y
+    pg3 = pg3 / y
+    listpg2 = c(listpg2, pg2)
+    listpg3 = c(listpg3, pg3)
+}
+print(length(listpg2))
+plot(seq(10, 1000, 50), listpg2)
